d38e32c1eb198c2cebd22bf82e346a742b8d1bb6
Floris Hafkenscheid <floris@hafkenscheid.com>
2024-09-17 10:23:01 +0200

    Got the transition for the sun filter working, just figuring out the background color transition.

--- js/main.js
+++ js/main.js
@@ -27,11 +27,26 @@ function updateBackgroundPosition() {
 
 function toggleTheme() {
     document.body.classList.toggle('light-theme');
-
+  
     const buttons = document.querySelectorAll('.theme_button');
     buttons.forEach(button => {
         button.classList.toggle('is-selected');
     });
+  
+    const sunContainer = document.querySelector('.sun-container');
+    sunContainer.style.opacity = '0';
+    backgroundElement.style.opacity = '0';
+
+    setTimeout(() => {
+        sunContainer.classList.toggle('sun-filter-light');
+        sunContainer.classList.toggle('sun-filter-dark');
+
+        backgroundElement.classList.toggle('background-dark');
+        backgroundElement.classList.toggle('background-light');
+
+        sunContainer.style.opacity = '1';
+        backgroundElement.style.opacity = '1';
+    }, 700);
 }
 
 window.toggleTheme = toggleTheme;
