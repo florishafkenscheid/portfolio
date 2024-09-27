93b47d0ff823e9261371bea95624d8643027f4ae
Floris Hafkenscheid <floris@hafkenscheid.com>
2024-09-27 18:42:37 +0200

    Added switch statement for the request URI

--- index.php
+++ index.php
@@ -3,12 +3,17 @@
 // require './views/layout/head.php';
 
 switch ($_SERVER['REQUEST_URI']) {
+    case '/projects':
+        require './views/projects/projects.view.php';
+    
     case '/info':
         require './views/info/info.view.php';
 
+    case '/contact':
+        require './views/contact/contact.view.php';
+
     default:
         require './views/home/home.view.php';
-    
 }
 
 ?>
