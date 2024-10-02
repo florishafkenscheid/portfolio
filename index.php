6124a519e799dea2201a7e03d50f27c2d59df2de
Floris Hafkenscheid <floris@hafkenscheid.com>
2024-10-02 09:37:22 +0200

    Completed routing.

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php
 
-include './controller/Controller.php';
-include './controller/HomeController.php';
-include './controller/ProjectsController.php';
-include './controller/InfoController.php';
-include './controller/ContactController.php';
+include './route/Route.php';
 
-switch ($_SERVER['REQUEST_URI']) {
-    case '/projects':
-        $projectsController = new ProjectsController();
-        $projectsController->redirect('projects');
-    
-    case '/info':
-        $infoController = new InfoController();
-        $infoController->redirect('info');
-
-    case '/contact':
-        $contactController = new ContactController();
-        $contactController->redirect('contact');
-
-    default:
-        $homeController = new HomeController();
-        $homeController->redirect('home');
-}
+$route = new Route();
+$route->contentToRender();
--- views/js/main.js
+++ views/js/main.js
@@ -34,12 +34,14 @@ const sunElement = document.querySelector('.sun-container img');
 const backgroundElement = document.querySelector('.background');
 
 function updateBackgroundPosition() {
-    const sunRect = sunElement.getBoundingClientRect();
-    const sunX = (sunRect.left + sunRect.width / 2) / window.innerWidth * 100;
-    const sunY = (sunRect.top + sunRect.height / 2) / window.innerHeight * 100;
+    if (sunElement != null && backgroundElement != null) {
+        const sunRect = sunElement.getBoundingClientRect();
+        const sunX = (sunRect.left + sunRect.width / 2) / window.innerWidth * 100;
+        const sunY = (sunRect.top + sunRect.height / 2) / window.innerHeight * 100;
 
-    backgroundElement.style.setProperty('--sun-x', `${sunX}%`);
-    backgroundElement.style.setProperty('--sun-y', `${sunY}%`);
+        backgroundElement.style.setProperty('--sun-x', `${sunX}%`);
+        backgroundElement.style.setProperty('--sun-y', `${sunY}%`);
+    }
 }
 
 setInterval(updateBackgroundPosition, 20); // Every 20 ms
@@ -62,5 +64,7 @@ window.toggleTheme = toggleTheme;
 window.transitionToPage = transitionToPage;
 
 if (page == '' || page == '/') {
-    setInterval(updateBackgroundPosition, 20); // Every 20 ms
+    if (sunElement != null) {
+        setInterval(updateBackgroundPosition, 20); // Every 20 ms
+    }
 }
