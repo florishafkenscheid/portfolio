9c5a8ebee811c3de648f58006fe604a313637b11
Floris Hafkenscheid <floris@hafkenscheid.com>
2024-10-28 10:35:24 +0100

    Wrote about me

--- views/info.view.php
+++ views/info.view.php
@@ -5,7 +5,16 @@ require './views/layout/head.php';
 
 <div class="info-div">
     <p class="info-p">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo quos doloribus, ut aut repellat incidunt. Possimus exercitationem maxime, ipsam distinctio iusto neque nesciunt quo saepe dicta officia. Quas, maxime earum?
+            Hey there! I'm Floris Hafkenscheid, a passionate Software Development student at Windesheim. My journey in coding started way back in middle school, and I haven't stopped since. 
+            <br>
+            <br>
+            Currently based in Dronten, Flevoland, I'm working towards becoming a specialist in both data science and artificial intelligence. My ultimate goal? Completing master's degrees in both fields to really push the boundaries of what's possible with technology.
+            <br>
+            <br>
+            When I'm not diving into code or exploring the latest tech innovations, you'll find me on the volleyball court or perfecting my swing on the golf course. And speaking of sports, I'm also a big motorsport enthusiast – currently riding an Aprilia RS 50 and dreaming about upgrading to a proper superbike someday.
+            <br>
+            <br>
+            Looking for a driven software developer who brings both technical skills and creative thinking to the table? Let's connect and create something amazing together.
     </p>
 </div>
 
